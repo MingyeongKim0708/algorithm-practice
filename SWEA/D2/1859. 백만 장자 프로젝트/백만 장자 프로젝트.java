25ea4bef044f06dd72c33b1fb5efdb6ba02207c2
MG <94948198+MingyeongKim0708@users.noreply.github.com>
2024-08-09 10:42:26 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 684 ms, Memory: 124,544 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -1,73 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Solution {
 
-	public void Solution() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		int T = Integer.parseInt(br.readLine());
+		int T = sc.nextInt();
 
 		for (int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(br.readLine());
+			int N = sc.nextInt();
 
-			// N일간의 가격 저장 배열
 			int[] price = new int[N];
-			StringTokenizer st = new StringTokenizer(br.readLine());
+
 			for (int i = 0; i < N; i++) {
-				price[i] = Integer.parseInt(st.nextToken());
+				price[i] = sc.nextInt();
 			}
 
-			// 현재 가격이 미래보다 싸다면 구매, 현재 가격이 미래보다 비싸다면 판매
-			// 구매한 것을 Queue에 저장
-			// 판매시 (현재가격 - Queue에 있는 값)의 합
-
-			Queue<Integer> queue = new LinkedList<>();
-			long gain = 0;
-
-			// i는 현재 가격 위치
-			for (int i = 0; i < N - 1; i++) {
-				// 그날의 구매 여부
-				boolean buy = false;
-
-				// j는 탐색하는 미래 가격 위치
-				for (int j = i + 1; j < N; j++) {
-					if (price[i] < price[j]) {
-						queue.add(price[i]);
-						buy = true;
-						break;
-					}
-				} // j
-				
-				// 현재 가격 기준 미래를 다 돌아봤지만 구매할 게 없음.
-				// queue에 사둔 것이 있다면 팔기.
-				// 구매한 날은 판매하면 안됨
-				if (!buy) {
-					while (!queue.isEmpty()) {
-						int sell = queue.poll();
-						gain += price[i] - sell;
-					}
-				}
-			} // i
+			// 100억이 나오면 int 범위를 벗어난다
+			long answer = 0;
 			
-			// 마지막날은 무조건 판매
-			while (!queue.isEmpty()) {
-				int sell = queue.poll();
-				gain += price[N - 1] - sell;
+			int[] max = new int[N];
+			max[N - 1] = price[N - 1];
+
+			for (int i = N - 2; i >= 0; i--) {
+				max[i] = Math.max(price[i], max[i+1]);
+				answer += max[i] - price[i];
 			}
+			
+			System.out.println("#" + tc + " " + answer);
 
-			System.out.printf("#%d %d\n", tc, gain);
-
-		} // tc
-
-	}// Solution
-
-	public static void main(String[] args) throws Exception {
-		new Solution().Solution();
-	}// main
-
-}// class
+		}
+	}
+}
