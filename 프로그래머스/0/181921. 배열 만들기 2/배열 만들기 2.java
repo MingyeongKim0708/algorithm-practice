eab61add1e73e21ebc3602f3282d197e014266e3
MG <94948198+MingyeongKim0708@users.noreply.github.com>
2024-08-21 17:19:41 +0900

    [level 0] Title: 배열 만들기 2, Time: 480.24 ms, Memory: 390 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+	public int[] solution(int l, int r) {
+
+
+		ArrayList<Integer> arr = new ArrayList<>();
+
+		for (int i = l; i <= r; i++) {
+			String str = Integer.toString(i);
+			// 정규식 사용
+			if (str.matches("[05]+")) { // []안에 들어간 문자를 하나 또는 많이 가지고 있다면
+				arr.add(i);
+			}
+
+		}
+
+		if (arr.isEmpty()) {
+			return new int[] { -1 };
+		}
+
+		// arr 정렬
+		Collections.sort(arr);
+
+		// ArrayList -> Array
+		int[] answer = new int[arr.size()];
+		for (int i = 0; i < arr.size(); i++) {
+			answer[i] = arr.get(i);
+		}
+
+		return answer;
+	}
+}
