bb811cb75c7697c6212fc1ff35716b909b9977f8
MG <94948198+MingyeongKim0708@users.noreply.github.com>
2024-08-28 11:37:34 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 142 ms, Memory: 20,144 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,31 +1,38 @@
 import java.util.Scanner;
 
 public class Solution {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		// 10개의 테스트 케이스
+
 		for (int tc = 1; tc <= 10; tc++) {
-			// 테스트 케이스 번호
-			int T = sc.nextInt();
+			sc.nextInt();
+			int N = sc.nextInt();
+			int M = sc.nextInt();
 
-			int num1 = sc.nextInt();
-			int num2 = sc.nextInt();
-			int result = powerCal(num1, num2);
-			System.out.printf("#%d %d\n", tc, result);
-		} // tc
+			int answer = power(N, M);
+			System.out.println("#" + tc + " " + answer);
 
-		sc.close();
-	}// main
+		}
+	}
 
-	static int powerCal(int num1, int num2) {
-		// Base case
-		if (num2 == 1)
-			return num1;
+	private static int power(int n, int m) {
+		if (m == 1) {
+			return n;
+		}
 
-		// 재귀 파트
-		return num1 * powerCal(num1, num2 - 1);
-	}// powerCal
+		if (m == 0) {
+			return 1;
+		}
 
-}// class
+		if (m % 2 == 0) {
+			int tmp = power(n, m / 2);
+			return tmp * tmp;
+		} else {
+			int tmp = power(n, (m - 1) / 2);
+			return tmp * tmp * n;
+		}
+
+	}
+
+}
