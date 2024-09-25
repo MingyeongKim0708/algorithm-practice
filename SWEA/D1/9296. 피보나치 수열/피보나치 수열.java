48027707468bc0e68a216f521b9e677ab79bb8cb
MG <94948198+MingyeongKim0708@users.noreply.github.com>
2024-09-25 18:53:29 +0900

    [D1] Title: 피보나치 수열, Time: 142 ms, Memory: 20,440 KB -BaekjoonHub

--- SWEA/D1/9296. 피보나치 수열/피보나치 수열.java
+++ SWEA/D1/9296. 피보나치 수열/피보나치 수열.java
@@ -1,24 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Solution {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		for (int i = 0; i < 5; i++) {
-			int N = sc.nextInt();
-			System.out.println(fibo(N));
-		}
-	}// main
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for (int tc = 1; tc <= 5; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			bw.write(fibo(N)+"\n");
+		} // tc
+		bw.flush();
+		bw.close();
+	}
 
 	private static long fibo(int n) {
-
-		long[] dp = new long[n + 1];
+		long dp[] = new long[n + 1];
 		dp[0] = 0;
 		dp[1] = 1;
 
 		for (int i = 2; i <= n; i++) {
-			dp[i] = dp[i - 2] + dp[i - 1];
+			dp[i] = dp[i - 1] + dp[i - 2];
 		}
 		return dp[n];
 	}
