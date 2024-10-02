5c4bcc0cb0c46e4c9d44660bc33876fa6bf94b2e
MG <94948198+MingyeongKim0708@users.noreply.github.com>
2024-10-02 19:15:51 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 267 ms, Memory: 35,716 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -18,21 +18,25 @@ public class Solution {
 			for (int i = 0; i < N; i++) {
 				num[i] = sc.nextInt();
 			}
-            
+
 			answer = 1;
 			dp = new int[N];
 			Arrays.fill(dp, 1); // 기본적으로 가장 첫번째 있는 값 하나만으로도 길이는 1이상
 			
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < i; j++) {
-					if (num[j] < num[i]) {
+					// 같다 조건이 있어야하나?? 문제가 헷갈림
+					if (num[j] <= num[i]) { // 뒤에 있는 숫자가 더 크다면 길이 늘릴 수 있음
 						dp[i] = Math.max(dp[i], dp[j] + 1);
 						// dp 마지막 값이 가장 길이가 긴 경우는 아닐 수 있으므로 최대값을 따로 찾아야한다
 						answer = Math.max(answer, dp[i]);
 					}
+//					System.out.println("i: " +i + " j: " + j);
+//					System.out.println("dp[" + i + "] : "+ Arrays.toString(dp));
+//					System.out.println("--------------");
 				}
 			}
-			
+//			System.out.println(Arrays.toString(dp));
 			System.out.println("#" + tc + " " + answer);
 		} // tc
 
@@ -40,3 +44,9 @@ public class Solution {
 	}
 
 }
+
+
+// 최대값을 가장 마지막 배열
+// dp : i보다 작은(작거나 같은) 최대 길이
+// 1일떄 : 0을봄 : 초기값 1 -> 1~N : 1
+// 2일때 : 
